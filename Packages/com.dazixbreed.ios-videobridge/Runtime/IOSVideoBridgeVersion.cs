e5ad2c42ffc3789c351217ec011922db676b3676
DAZIxBREED <cystacae@gmail.com>
2026-08-10 07:51:23 -0400

    v0.1.1 bump runtime version

--- Packages/com.dazixbreed.ios-videobridge/Runtime/IOSVideoBridgeVersion.cs
+++ Packages/com.dazixbreed.ios-videobridge/Runtime/IOSVideoBridgeVersion.cs
@@ -2,6 +2,6 @@ namespace DAZIxBREED.IOSVideoBridge
 {
     public static class IOSVideoBridgeVersion
     {
-        public const string Value = "0.1.0-phase1";
+        public const string Value = "0.1.1";
     }
 }
