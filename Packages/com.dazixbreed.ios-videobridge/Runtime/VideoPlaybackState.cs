198f5ac310fe37cc42ac2b4bf3dc8ee92b196243
DAZIxBREED <cystacae@gmail.com>
2026-08-10 07:46:53 -0400

    v0.1.1 formalize playback states

--- Packages/com.dazixbreed.ios-videobridge/Runtime/VideoPlaybackState.cs
+++ Packages/com.dazixbreed.ios-videobridge/Runtime/VideoPlaybackState.cs
@@ -2,17 +2,15 @@ namespace DAZIxBREED.IOSVideoBridge
 {
     public enum VideoPlaybackState
     {
-        Idle,
-        Loaded,
-        Preparing,
-        Prepared,
-        Playing,
-        Paused,
-        Stopped,
-        Completed,
-        Stalled,
-        Recovering,
-        Error,
-        Released
+        Idle = 0,
+        Loading = 1,
+        Preparing = 2,
+        Ready = 3,
+        Playing = 4,
+        Paused = 5,
+        Buffering = 6,
+        Recovering = 7,
+        Failed = 8,
+        Stopped = 9
     }
 }
